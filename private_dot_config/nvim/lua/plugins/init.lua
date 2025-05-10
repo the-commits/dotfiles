c5854231cb8eec2c73ddac07c9ee900bcaf4f614
Magnus Åberg <himself@magnusaberg.me>
2025-05-10 15:13:01 +0200

    Update .config/nvim/lua/plugins/init.lua

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -1,3 +1,11 @@
 return {
   { url = "https://github.com/liuchengxu/vim-which-key.git"},
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+  },
 }
