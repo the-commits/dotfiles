bdaddc0d9aac17a312f0b3b6be75d36d786341e1
The commits <12031896+the-commits@users.noreply.github.com>
2025-05-13 19:30:46 +0200

    Update .config/hypr/scripts/disturbed-mode.sh

--- private_dot_config/hypr/scripts/executable_disturbed-mode.sh
+++ private_dot_config/hypr/scripts/executable_disturbed-mode.sh
@@ -1,24 +1,35 @@
 #!/usr/bin/env bash
 
-
 WORKING_HOUR_START="08:00"
 WORKING_HOUR_END="17:00"
 CURRENT_TIME=$(date +%H:%M)
 CURRENT_DAY=$(date +%w)
 
-check_if_work_time()
-{
-  if [[ "$CURRENT_DAY" < "6" ]]; then
-    if [[ "$CURRENT_TIME"  > "$WORKING_HOUR_START" ]] && [[ "$CURRENT_TIME" < "$WORKING_HOUR_END" ]]; then
-      notify-send --app-name "disturbed-mode.sh" -t 5000 -u normal 'Slår på "Do Not Disturb"'
-      makoctl mode -s do-not-disturb
+get_current_mode() {
+  makoctl mode
+}
+
+check_if_work_time() {
+  local new_mode
+  local current_mode=$(get_current_mode)
+
+  if [[ "$CURRENT_DAY" -lt "6" ]]; then
+    if [[ "$CURRENT_TIME" > "$WORKING_HOUR_START" ]] && [[ "$CURRENT_TIME" < "$WORKING_HOUR_END" ]]; then
+      new_mode="do-not-disturb"
     else
-      notify-send --app-name "disturbed-mode.sh" -t 5000 -u low 'Slår av "Do not Disturb"'
-      makoctl mode -s default 
+      new_mode="default"
     fi
   else
-    notify-send --app-name "disturbed-mode.sh" -t 5000 -u low 'Slår av "Do not Disturb"'
-    makoctl mode -s default 
+    new_mode="default"
+  fi
+
+  if [[ "$new_mode" != "$current_mode" ]]; then
+    makoctl mode -s "$new_mode"
+    if [[ "$new_mode" == "do-not-disturb" ]]; then
+      notify-send --app-name "disturbed-mode.sh" -t 5000 -u normal 'Slår på "Do Not Disturb"'
+    else
+      notify-send --app-name "disturbed-mode.sh" -t 5000 -u low 'Slår av "Do Not Disturb"'
+    fi
   fi
 }
 
